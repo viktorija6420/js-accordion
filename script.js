fa294e5cf73806e270d6d7ed9b1764ba848c7b44
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-12-14 21:33:20 -0500

    fix issue with li not opening in js and css

---
+++ script.js
@@ -0,0 +1,17 @@
+const accordion = document.querySelector('.accordion');
+const items = accordion.querySelectorAll('li');
+const questions = accordion.querySelectorAll('.question');
+
+function toggleAccordion(){
+  const thisItem = this.parentNode;
+console.log(thisItem);
+items.forEach(item =>{
+  if (thisItem == item) {
+   thisItem.classList.toggle('open');
+    return;
+  }
+  item.classList.remove('open');
+});
+}
+
+questions.forEach(question => question.addEventListener('click', toggleAccordion));
